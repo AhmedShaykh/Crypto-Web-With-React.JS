c8f0aa4b57da6b9b18628549f2b47d839db5c2f2
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-01-28 22:06:00 +0500

    Now Ready To Push!

---
+++ build/precache-manifest.67daa54818089163145dde4d497fb085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a01072181b41856faf5dac1b8fb9b5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81562b3b1960769eccf0",
+    "url": "/static/css/main.f6a1ffa7.chunk.css"
+  },
+  {
+    "revision": "b61a54856621b39152a9",
+    "url": "/static/js/2.53558318.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.53558318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81562b3b1960769eccf0",
+    "url": "/static/js/main.6c0f1082.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "2a4ac9a43a95d361f50010e2f85039b5",
+    "url": "/static/media/logo.2a4ac9a4.svg"
+  }
+]);
