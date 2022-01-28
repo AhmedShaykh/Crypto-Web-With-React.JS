7d89d6b439b43a96900fd6c9f0face906a1855bf
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-01-28 22:04:28 +0500

    Now Ready To Push!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Crypto-Web3-With-React.JS
+# Crypto WEB 3.0 With React.JS
+
+Hello Welcome To Blockchain & Metaverse World . We Explore Web 3.0 , De-Fi & NFT's With Crypto
+
+http://crypto-web3-by-ahmed.surge.sh/
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          Hello Welcome To Blockchain & Metaverse World.<br/>We Explore Web 3.0 , De-Fi & NFT's With Cryptocurrencies
+        </h3>
+        <a
+          className="App-link"
+          href="https://coinmarketcap.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          We Love Bitcoin
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
